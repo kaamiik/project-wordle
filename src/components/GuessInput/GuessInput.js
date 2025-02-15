6115d974a6cee8dc994baa15e62f2400072516bd
Kamran Kiani <kiani.km@gmail.com>
2025-02-15 16:02:49 +0330

    add all the codes

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function GuessInput({ gameStatus, handleSubmitGuess }) {
+  const [tentativeGuess, setTentativeGuess] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleSubmitGuess(tentativeGuess);
+    setTentativeGuess("");
+  }
+  return (
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        disabled={gameStatus !== "running"}
+        required={true}
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        value={tentativeGuess}
+        onChange={(event) => {
+          const nextGuess = event.target.value.toUpperCase();
+          setTentativeGuess(nextGuess);
+        }}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
